af226644bf32bcdc0b29363481b9334fee56ff82
Valentin Fomenco <fomencovalentin83@gmail.com>
2018-03-23 18:18:27 +0200

    new repository

---
+++ speakgoodbye.js
@@ -0,0 +1,11 @@
+(function (){
+
+    var byeSpeaker = {};
+
+    byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }
+    var speakWord = "Good Bye";
+    window.byeSpeaker = byeSpeaker;
+
+})();
---
+++ speakhello.js
@@ -0,0 +1,12 @@
+(function (){
+
+    var helloSpeaker = {};
+
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }
+
+    var speakWord = "Hello";
+
+    window.helloSpeaker = helloSpeaker;
+})();
